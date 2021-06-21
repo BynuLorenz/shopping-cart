afa40df471a5298534d88d4d633493889fd5a540
bynulorenz <bynu022@gmail.com>
2021-06-21 14:01:10 +0530

    Code formatting.

--- Code/shopping-cart/src/main/java/com/gapstars/assessment/shoppingcart/config/SwaggerConfig.java
+++ Code/shopping-cart/src/main/java/com/gapstars/assessment/shoppingcart/config/SwaggerConfig.java
@@ -1,24 +1,4 @@
-// *************************************************************************************************
-//
-// PROJECT : ugc-std-reg
-// PRODUCT : UGC Student Registration
-// CLASS : SwaggerConfig
-// PURPOSE : UGC Student Registration
-// ************************************************************************************************
-//
-// Copyright(C) 2019 SprintLabs (Pvt) Ltd.
-// All rights reserved.
-//
-// THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
-// SprintLabs (Pvt) Ltd.
-//
-// This copy of the Source Code is intended for SprintLabs (Pvt) Ltd 's internal use only
-// and is
-// intended for view by persons duly authorized by the management of SprintLabs (Pvt) Ltd. 
-//No part of this file may be reproduced or distributed in any form or by any
-// means without the written approval of the Management of SprintLabs (Pvt) Ltd.
-//
-// *************************************************************************************************
+
 
 package com.gapstars.assessment.shoppingcart.config;
 
--- Code/shopping-cart/src/main/java/com/gapstars/assessment/shoppingcart/dao/entity/GeneratedIdEntity.java
+++ Code/shopping-cart/src/main/java/com/gapstars/assessment/shoppingcart/dao/entity/GeneratedIdEntity.java
@@ -1,21 +1,4 @@
-// *************************************************************************************************
-// PRODUCT : cargills-cash
-// CLASS : GeneratedBaseEntity
-// ************************************************************************************************
-//Copyright(C) 2020 Fortunaglobal (PRIVATE) LIMITED
-/* All rights reserved.
- *
- * THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
- * Fortunaglobal(PRIVATE) LIMITED.
- *
- * This copy of the Source Code is intended for Fortunaglobal (PRIVATE) LIMITED's
- * internal use only and is intended for view by persons duly authorized by the
- * management of Fortunaglobal (PRIVATE) LIMITED.
- * No part of this file may be reproduced or distributed in any form or by any
- * means without the written approval of the Management of
- * Fortunaglobal (PRIVATE) LIMITED.
- */
-// *************************************************************************************************
+
 
 package com.gapstars.assessment.shoppingcart.dao.entity;
 
