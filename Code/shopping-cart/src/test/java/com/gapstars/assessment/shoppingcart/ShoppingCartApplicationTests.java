04147d1fc04a4bdbe0a7744a08063be3e08bc830
bynulorenz <bynu022@gmail.com>
2021-06-27 12:05:49 +0530

    Addede more Application Tests.

--- Code/shopping-cart/src/test/java/com/gapstars/assessment/shoppingcart/ShoppingCartApplicationTests.java
+++ Code/shopping-cart/src/test/java/com/gapstars/assessment/shoppingcart/ShoppingCartApplicationTests.java
@@ -1,12 +1,31 @@
 package com.gapstars.assessment.shoppingcart;
 
+import com.gapstars.assessment.shoppingcart.common.dto.CustomerDto;
+import com.gapstars.assessment.shoppingcart.common.dto.ProductDto;
 import com.gapstars.assessment.shoppingcart.common.property.ShoppingCartProperties;
+import com.gapstars.assessment.shoppingcart.controller.payload.response.AddProductsResponse;
 import com.gapstars.assessment.shoppingcart.controller.payload.response.CartAmountResponse;
+import com.gapstars.assessment.shoppingcart.controller.payload.response.CustomerResponse;
+import com.gapstars.assessment.shoppingcart.controller.payload.response.ProductResponse;
+import com.gapstars.assessment.shoppingcart.dao.entity.CartEntity;
+import com.gapstars.assessment.shoppingcart.dao.entity.CustomerEntity;
+import com.gapstars.assessment.shoppingcart.dao.entity.ProductEntity;
+import com.gapstars.assessment.shoppingcart.dao.repository.CartRepository;
+import com.gapstars.assessment.shoppingcart.dao.repository.CustomerRepository;
+import com.gapstars.assessment.shoppingcart.dao.repository.ProductRepository;
 import com.gapstars.assessment.shoppingcart.service.CustomerService;
+import com.gapstars.assessment.shoppingcart.service.ProductService;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.swing.text.html.Option;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -16,9 +35,80 @@ class ShoppingCartApplicationTests {
 	CustomerService customerService;
 	@Autowired
 	ShoppingCartProperties properties;
+	@Autowired
+	CustomerRepository customerRepository;
+	@Autowired
+	ProductRepository productRepository;
+	@Autowired
+	CartRepository cartRepository;
+	@Autowired
+	ProductService productService;
 
 	@Test
-	void checkCartAmounts() {
+	void createCustomer () {
+
+		CustomerDto customerDto = new CustomerDto();
+		customerDto.setFirstName( "Test" );
+		customerDto.setLastName( "Test" );
+		CustomerResponse response = customerService.createCustomer( customerDto ) ;
+		Assertions.assertThat( response.getId() != 0 );
+
+	}
+
+	@Test
+	@Transactional
+	void findCustomerById () {
+
+		Optional<CustomerEntity> entity= customerRepository.findById( 1L );
+		assertEquals( entity.get().getFirstName(), "John W." );
+	}
+
+	@Test
+	@Transactional
+	void findCartById () {
+
+		Optional<CartEntity> cartEntity = cartRepository.findById( 1L );
+		assertEquals( cartEntity.get().getId(), 1L );
+	}
+
+	@Test
+	@Transactional
+	void findCartIdByCustomer () {
+
+		Optional<CustomerEntity> customerEntity = customerRepository.findById( 1L );
+		assertEquals( customerEntity.get().getCartEntity().getId(), 1L );
+	}
+
+	@Test
+	@Transactional
+	void findProductById () {
+
+		Optional<ProductEntity> productEntity = productRepository.findById( 1L );
+		assertEquals( productEntity.get().getProductName(), "KITKAT" );
+	}
+
+	@Test
+	void addProductsToCart () {
+
+		List<ProductDto> productDtos = new ArrayList<>();
+		ProductDto dto = new ProductDto();
+		dto.setProductId( 1L );
+		productDtos.add( dto );
+
+		AddProductsResponse response = customerService.addProductsToCart( productDtos, 1L );
+		assertEquals( response.getCartId(), 1L );
+	}
+
+	@Test
+	@Transactional
+	void findProductTitleByProductIdId () {
+
+		Optional<ProductEntity> productEntity = productRepository.findById( 1L );
+		assertEquals( productEntity.get().getProductTitle().getProductTitleName(), "CHOCOLATE" );
+	}
+
+	@Test
+	void checkCartAmounts () {
 
 		CartAmountResponse response = customerService.calculateCartAmounts(2L);
 		//assertEquals(response.getTotalCartAmount(), new BigDecimal(1353.15));
@@ -26,8 +116,46 @@ class ShoppingCartApplicationTests {
 	}
 
 	@Test
-	void checkPropertyFile() {
-		assertEquals( properties.getGenericShippingFee(), new BigDecimal( 100.00 ) );
+	void checkPropertyFile () {
+		assertEquals( properties.getGenericShippingFee(), new BigDecimal( 100 ) );
+	}
+
+	@Test
+	void calculateCartAmounts () {
+
+		CartAmountResponse response = customerService.calculateCartAmounts( 2L );
+		Assertions.assertThat( response.getTotalCartAmount() != BigDecimal.ZERO );
+	}
+
+	@Test
+	void getAllCustomers () {
+
+		List<CustomerResponse> responses = customerService.getAllCustomers();
+		Assertions.assertThat( responses.size() != 0 );
+	}
+
+	@Test
+	void getAllProducts () {
+
+		List<ProductResponse> responses = productService.getAllProducts();
+		Assertions.assertThat( responses.size() != 0 );
+	}
+
+	@Test
+	void addProduct () {
+
+		Optional<ProductEntity> productEntity = productRepository.findByProductName( "OREO" );
+		assertNotNull( productEntity );
+
+		ProductDto dto = new ProductDto();
+		dto.setPrice( new BigDecimal( 100.00) );
+		dto.setProductName( "Creamy" );
+		dto.setProductTitle( productEntity.get().getProductTitle().getId() );
+		dto.setProductQuantity( new BigDecimal( 500 ) );
+		dto.setTax( new BigDecimal( 3.2 ) );
+
+		ProductResponse response = productService.createProduct( dto  );
+		assertNotNull( response.getProductId() );
 	}
 
 }
